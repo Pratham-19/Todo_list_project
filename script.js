328070e30b26e40162d95bb5e62536d8c1a7b37c
Pratham-19 <prathamsinghal2003@gmail.com>
2022-06-16 16:59:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("button").onclick = function () {
+    on()
+};
+
+function hello() {
+    window.alert("Hi,You clicked");
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("loginBox").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("loginBox").style.display = "none";
+}
